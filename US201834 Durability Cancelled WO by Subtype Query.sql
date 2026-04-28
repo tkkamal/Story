1034284ae9d62706913850ba4224102f0177caa5
tkkamal <kamalahsan@hotmail.com>
2026-04-28 11:36:33 -0500

    modeified test script

--- US201834 Durability Cancelled WO by Subtype Query.sql
+++ US201834 Durability Cancelled WO by Subtype Query.sql
@@ -41,18 +41,18 @@ INNER JOIN (
            "Cur_Area_Nm"
     FROM "Dim_Corp_Hier"
     WHERE "Is_Current" = TRUE
-    and "Cur_Div_Nbr" =3733
+   and "Cur_Div_Nbr" in (3430,4691,4692,4174,4581)
 ) dh
 ON CAST(wom."Div_Nbr" AS DECIMAL) = dh."Cur_Div_Nbr"
 
 WHERE UPPER(wom."Asset_Nbr") LIKE '%PARENT%'
   AND UPPER(wom."Status") NOT IN ('SOLD', 'SALVAGE')
   AND wom."Work_Order_Status_Nm" IN ('Completed', 'Pending P-Card Completion', 'Pending QCI', 'Closed')
-  AND CAST(wom."Actual_Start_Dt" AS DATE) BETWEEN '2026-04-12' AND '2026-04-18'
+  AND CAST(wom."Actual_Completion_Dt" AS DATE) BETWEEN '2026-01-25' AND '2026-02-07'
 
 GROUP BY
     dh."Cur_Area_Nbr" || '-' || dh."Cur_Area_Nm",
     dh."Cur_BU_Nbr"   || '-' || dh."Cur_BU_Desc",
     wom."Div_Nbr"     || '-' || dh."Cur_Div_Nm"
 
-LIMIT 10
+order by  wom."Div_Nbr"     || '-' || dh."Cur_Div_Nm" 
